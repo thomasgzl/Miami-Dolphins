8244ed35062788283de748d1af76d9e11a93393b
christophepetit17 <cris17.mac@gmail.com>
2018-10-10 17:09:36 +0200

    first commit page news zoom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NewsZoom00 from './components/NewsZoom00';
+import NewsZoom01 from './components/NewsZoom01';
+import NewsZoom02 from './components/NewsZoom02';
+import NewsZoom03 from './components/NewsZoom03';
+import NewsZoom04 from './components/NewsZoom04';
+import NewsZoom05 from './components/NewsZoom05';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      
+      <BrowserRouter>
+      
+        <Switch>
 
-      </div>
+          <Route path="/" component={NewsZoom00} exact/>
+          <Route path="/newszoom01" component={NewsZoom01}/>
+          <Route path="/newszoom02" component={NewsZoom02}/>
+          <Route path="/newszoom03" component={NewsZoom03}/>
+          <Route path="/newszoom04" component={NewsZoom04}/>
+          <Route path="/newszoom05" component={NewsZoom05}/>
+
+        </Switch>
+        
+      </BrowserRouter>
+      
     );
   }
 }
