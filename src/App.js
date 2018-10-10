deb0c031a11e08a29de59a4b8498ab5b58861972
Vivien POUGET <vivien.pouget@hotmail.fr>
2018-10-10 14:28:42 +0200

    Bandeau, premiere version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Bandeau from './components/Bandeau';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Bandeau/>
       </div>
     );
   }
