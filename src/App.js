11f8354df6c200e38701db9edecc4edbb11ed9b5
Julien GARNON <garnon@hotmail.com>
2018-10-10 15:58:16 +0200

    navbar v1.0 avec liens non à jour, fond transparent, menus burger et logo-home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarMain from './component/NavbarMain';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+<NavbarMain />
       </div>
     );
   }
