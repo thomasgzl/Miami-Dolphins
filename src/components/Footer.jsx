8dc889533fabe3e0a13d394efe2ec6d4a7d8d55c
Alissone Neyret <alissone.neyret@gmail.com>
2018-10-11 11:55:15 +0200

    modif logo reseaux

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,16 +23,16 @@ class Footer extends Component {
                         <img src="https://i.imgur.com/JXboZ1e.png" alt="logo" />
                     </Col>
                     <Col md className="lien_section_footer">
-                        <a href="https://www.facebook.com/MiamiDolphins/" target="_blank"><img className="logo_reseau" src="https://res.cloudinary.com/dzgbfpr45/image/upload/v1539184102/projet2%20-%20R%C3%A9%27action/logo_facebook.png" alt="logo"></img></a>
+                        <a href="https://www.facebook.com/MiamiDolphins/" target="_blank"><img className="logo_reseau_fb" src="https://i.imgur.com/jrjKEda.png" alt="logo"></img></a>
                     </Col>
                     <Col md className="lien_section_footer">
-                        <a href="https://twitter.com/miamidolphins" target="_blank"><img className="logo_reseau" src="https://res.cloudinary.com/dzgbfpr45/image/upload/v1539184101/projet2%20-%20R%C3%A9%27action/logo_twitter.png" alt="logo"></img></a>
+                        <a href="https://twitter.com/miamidolphins" target="_blank"><img className="logo_reseau_twitter" src="https://i.imgur.com/5CN4Mu7.png" alt="logo"></img></a>
                     </Col>
                     <Col md className="lien_section_footer">
-                        <a href="https://www.instagram.com/miamidolphins/?hl=fr" target="_blank"><img className="logo_reseau" src="https://res.cloudinary.com/dzgbfpr45/image/upload/v1539184102/projet2%20-%20R%C3%A9%27action/logo_insta.png" alt="logo"></img></a>
+                        <a href="https://www.instagram.com/miamidolphins/?hl=fr" target="_blank"><img className="logo_reseau_insta" src="https://i.imgur.com/jIz2oU2.png" alt="logo"></img></a>
                     </Col>
                     <Col md className="lien_section_footer">
-                        <a href="https://www.youtube.com/channel/UCHUSfEzpSRkUUsRkk_aJwDw" target="_blank"><img className="logo_reseau" src="https://res.cloudinary.com/dzgbfpr45/image/upload/v1539184941/projet2%20-%20R%C3%A9%27action/1280px-YouTube_full-color_icon__2017_.svg.png" alt="logo"></img></a>
+                        <a href="https://www.youtube.com/channel/UCHUSfEzpSRkUUsRkk_aJwDw" target="_blank"><img className="logo_reseau_youtube" src="https://i.imgur.com/UDBghCh.png" alt="logo"></img></a>
                     </Col>
                 </Row>
             </Container>
