14470691af1cfc234de1f4876ffbb6c8f92639d2
Alissone <42072495+alissone-neyret@users.noreply.github.com>
2018-10-12 09:32:08 +0200

    Update NavbarMain.jsx

--- src/components/NavbarMain.jsx
+++ src/components/NavbarMain.jsx
@@ -25,16 +25,7 @@ class NavbarMain extends Component {
     this.state = {
       isOpen: false
     }
-    /*Navbar.propTypes = {
-      light: PropTypes.bool,
-      dark: PropTypes.bool,
-      fixed: PropTypes.string,
-      color: PropTypes.string,
-      role: PropTypes.string,
-      expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-      tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
-      // pass in custom element to use
-    };*/
+    
   }
   toggle() {
     this.setState({
