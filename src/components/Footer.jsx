a485c8b9527476337f33177bce0f50d140a9c827
Julien GARNON <garnon@hotmail.com>
2018-10-22 16:23:20 +0200

    clean du code avec remise du target dans le fotter mais ajout des sécurités demandées

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import './Footer.css';
 
+
 class Footer extends Component {
     render() { 
         return (
@@ -11,16 +12,16 @@ class Footer extends Component {
                         <img src="https://i.imgur.com/JXboZ1e.png" alt="logo" />
                     </Col>
                     <Col lg="2" sm="3" className="lien_section_footer">
-                        <a href="https://www.facebook.com/MiamiDolphins/" target=""><img className="logo_reseau_fb" src="https://i.imgur.com/jrjKEda.png" alt="logo"></img></a>
+                        <a href="https://www.facebook.com/MiamiDolphins/" target="_blank" rel="noreferrer noopener"><img className="logo_reseau_fb" src="https://i.imgur.com/jrjKEda.png" alt="logo"></img></a>
                     </Col>
                     <Col lg="2" sm="3" className="lien_section_footer">
-                        <a href="https://twitter.com/miamidolphins" target=""><img className="logo_reseau_twitter" src="https://i.imgur.com/5CN4Mu7.png" alt="logo"></img></a>
+                        <a href="https://twitter.com/miamidolphins" target="_blank" rel="noreferrer noopener"><img className="logo_reseau_twitter" src="https://i.imgur.com/5CN4Mu7.png" alt="logo"></img></a>
                     </Col>
                     <Col lg="2" sm="3" className="lien_section_footer">
-                        <a href="https://www.instagram.com/miamidolphins/?hl=fr" target=""><img className="logo_reseau_insta" src="https://i.imgur.com/jIz2oU2.png" alt="logo"></img></a>
+                        <a href="https://www.instagram.com/miamidolphins/?hl=fr" target="_blank" rel="noreferrer noopener"><img className="logo_reseau_insta" src="https://i.imgur.com/jIz2oU2.png" alt="logo"></img></a>
                     </Col>
                     <Col lg="2" sm="3" className="lien_section_footer">
-                        <a href="https://www.youtube.com/channel/UCHUSfEzpSRkUUsRkk_aJwDw" target=""><img className="logo_reseau_youtube" src="https://i.imgur.com/UDBghCh.png" alt="logo"></img></a>
+                        <a href="https://www.youtube.com/channel/UCHUSfEzpSRkUUsRkk_aJwDw" target="_blank" rel="noreferrer noopener"><img className="logo_reseau_youtube" src="https://i.imgur.com/UDBghCh.png" alt="logo"></img></a>
                     </Col>
                 </Row>
             </Container>
