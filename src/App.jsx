70500afca8abc2db00d7b5a76d03bfa8a3ad7c11
Julien GARNON <garnon@hotmail.com>
2018-10-23 22:00:18 +0200

    remise de App

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import NavbarMain from './components/NavbarMain';
+import Bandeau from './components/Bandeau';
+import Footer from './components/Footer';
+import News from './components/News';
+import Home from './components/Home';
+
+import { Route, Switch } from "react-router-dom";
+import NewsZoom from './components/NewsZoom';
+import Calendrier from './components/Calendrier';
+import Historique from './components/Historique';
+import Equipe from './components/Equipe';
+import ProfilJoueur from './components/ProfilJoueur';
+
+
+import AdminJoueurs from './components/AdminJoueurs';
+import AdminCalendrier from './components/AdminCalendrier';
+import AdminNews from './components/AdminNews';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Bandeau />
+        <NavbarMain />   
+                <Switch>
+                  <Route exact path="/" component={Home}/>
+                  <Route path="/historique" component={Historique}/>
+                  <Route path="/equipe" component={Equipe}/>
+                  <Route path="/news" component={News}/>
+                  <Route path="/calendrier" component={Calendrier}/>  
+                  <Route path="/newszoom/:id" component={NewsZoom}/>
+                  <Route path="/historique" component={Historique}/>
+                  <Route path="/adminjoueurs" component={AdminJoueurs}/>
+                  <Route path="/equipe" component={Equipe}/>
+                  <Route path="/profiljoueur" component={ProfilJoueur}/>
+                  <Route path="/admincalendrier" component={AdminCalendrier}/>
+                  <Route path="/admin-news" component={AdminNews}/>
+                </Switch>
+         <Footer />
+         </div>    
+    );
+  }
+}
+
+export default App;
