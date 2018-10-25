cb26c6e32831cee87fd90cd7dd14f4758f9af18f
Thomas Gonzalez <thomasgonzalez.pro@gmail.com>
2018-10-25 14:06:41 +0200

    test

---
+++ src/components/CarouselBandeau.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+
+class Example extends Component {
+  render() { 
+    return ( 
+
+      <div></div>
+
+     );
+  }
+}
+ 
+export default Example;
